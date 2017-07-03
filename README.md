dd92c3215b14e971725a548b385861bf9996ec3a
LeetCodes <michael@leetcodes.com>
2017-07-02 22:34:25 -0400

    Added support for temp-mail.org
    Added support for temp-mail.org, with http authentication using API
    credentials. Quick and dirty more for personal use than anything..

--- README.md
+++ README.md
@@ -1,12 +1,50 @@
 # temp-mail [![Build Status](https://travis-ci.org/xomyaq/temp-mail.svg?branch=master)](https://travis-ci.org/xomyaq/temp-mail)
 
-Simple node.js API wrapper for temp-mail.ru
+Simple node.js API wrapper for temp-mail.ru and small modification by me (LeetCodes) to work with temp-mail.org
+
 
 ## Install
 ```
-npm install temp-mail
+npm install https://github.com/LeetCodes/temp-mail.git
 ```
 
+## Usage
+```
+const TempMail = require('temp-mail')();
+```
+
+## Example of temp-mail.org usage
+Temp-mail.org requires API username and password authentication, free credentials can be obtained by emailing them.
+Each function returns a Promise
+generateEmail, getInbox, deleteMail
+Quick and dirty modification, more for personal use than anything..
+Note: The temp-mail.org SSL certificate is currently expired so you need to disable certificate rejection, it defaults to HTTP
+```
+const TempMail = require('temp-mail')({
+		domain: 'org',
+		username: '11111111-2222-3333-4444-555555555555',
+		password: '123456789',
+		// Optional HTTPS when not required, defaults to false
+		https: true
+	});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+TempMail.generateEmail()
+	.then(result => {
+			console.log("Email generated:",result);
+			TempMail.getInbox(result)
+				.then(result => console.log(result))
+		}
+	).catch(function(e) {
+		console.log(e);
+	});
+let email = TempMail.getInbox('33ef0r5@buy003.com');
+email.then(function(result){
+	console.log(result);
+	}).catch(function(e) {
+		console.log(e);
+	});
+```
 ## License
 
 MIT
--- index.js
+++ index.js
@@ -1,107 +1,111 @@
-const crypto = require('crypto');
-const https = require('https');
-
-
-/**
- * @type {string}
- * @const
- */
-const API_URL = 'https://api.temp-mail.ru';
+const	crypto = require('crypto'),
+		https = require('https'),
+		http = require('http')
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 /**
- * Makes GET request
- * @param {string} url
- * @returns {Promise}
+ * Description
+ * @method TempMail
+ * @param {} config
+ * @return ObjectExpression
  */
-function get(url) {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, (res) => {
-        if (res.statusCode < 200 || res.statusCode > 299) {
-          reject(new Error(`Request failed: ${res.statusCode}`));
-        }
+function TempMail(conf={domain: 'ru', https: false}) {
+	const API_URL = () => ( conf.domain === 'org' ? 'api2.temp-mail.org' : 'api.temp-mail.ru' );
+	const API_USERNAME = conf.username;
 
-        let data = '';
+	/**
+	 * Description
+	 * @method get
+	 * @param {} url
+	 * @return NewExpression
+	 */
+	function get(url) {
+		var h = () => ( ( typeof conf.username !== typeof undefined && typeof conf.password !== typeof undefined && conf.domain !== 'ru' ) ? { 'Authorization': 'Basic ' + new Buffer(conf.username + ':' + conf.password).toString('base64') } : { } ),
+			options = {
+				host: API_URL(),
+				port: ( (conf.domain === 'ru') ? 443 : (conf.https) ? 443 : 80 ),
+				path: url,
+				headers: h()
+			};
+		return new Promise((resolve, reject) => {
+			((( conf.domain === 'org' ) ? (( typeof conf.username !== typeof undefined && typeof conf.password !== typeof undefined ) ? true : (() => { reject(new Error('API Authentication required for temp-mail.org')); })()) : false ));
+			( (conf.domain === 'ru') ? https : (conf.https) ? https : http )
+				.get(options, (res) => {
+					if (res.statusCode !== 404 && (res.statusCode < 200 || res.statusCode > 299)) {
+						reject(new Error(`Request failed: ${res.statusCode}`));
+					}
 
-        res
-          .on('data', (chunk) => { data += chunk; })
-          .on('end', () => resolve(data));
-      })
-      .on('error', reject);
-  });
+					let data = '';
+
+					res
+						.on('data', (chunk) => { data += chunk; })
+						.on('end', () => resolve(data));
+				})
+				.on('error', reject);
+		});
+	}
+	/**
+	 * Description
+	 * @method getEmailHash
+	 * @param {} email
+	 * @return CallExpression
+	 */
+	function getEmailHash(email) {
+		return crypto.createHash('md5').update(email).digest('hex');
+	}
+	/**
+	 * Description
+	 * @method getRandomEmail
+	 * @param {} domains
+	 * @param {} length
+	 * @param {} prefix
+	 * @return BinaryExpression
+	 */
+	function getRandomEmail(domains, len = 7, prefix = '') {
+		const alfabet = '1234567890abcdefghijklmnopqrstuvwxyz';
+
+		let name = !prefix ? '' : `${prefix}-`;
+
+		for (let i = 0; i < len; i++) {
+			const randomChar = Math.round(Math.random() * (alfabet.length - 1));
+			name += alfabet.charAt(randomChar);
+		}
+
+		const domain = domains[Math.floor(Math.random() * domains.length)];
+
+		return name + domain;
+	}
+	/**
+	 * Description
+	 * @method getAvailableDomains
+	 * @return CallExpression
+	 */
+	function getAvailableDomains() {
+		return get(`/request/domains/format/json/`).then(JSON.parse);
+	}
+
+    return {
+        generateEmail: (len, prefix) => {
+			return getAvailableDomains()
+				.then(availableDomains => getRandomEmail(availableDomains, len, prefix));
+		},
+		getInbox: (email) => {
+			return new Promise((resolve, reject) => {
+				if (!email) {
+					return reject('Please specify email');
+				}
+				resolve(get(`/request/mail/id/${getEmailHash(email)}/format/json/`).then(JSON.parse));
+			});
+		},
+		deleteMail: (mailId) => {
+			return new Promise((resolve, reject) => {
+				if (!mailId) {
+					return reject('Please specify mail identifier');
+				}
+				resolve(get(`/request/delete/id/${mailId}/format/json/`).then(JSON.parse));
+			});
+		}
+    };
 }
 
-/**
- * Generates MD5 hash from email
- * @param {string} email
- * @returns {string}
- */
-function getEmailHash(email) {
-  return crypto.createHash('md5').update(email).digest('hex');
-}
-
-/**
- * Generates random email in given domains
- * @param {Array} domains
- * @param {number} [len=7]
- * @param {string} prefix
- * @returns {string}
- */
-function getRandomEmail(domains, len = 7, prefix = '') {
-  const alfabet = '1234567890abcdefghijklmnopqrstuvwxyz';
-
-  let name = !prefix ? '' : `${prefix}-`;
-
-  for (let i = 0; i < len; i++) {
-    const randomChar = Math.round(Math.random() * (alfabet.length - 1));
-    name += alfabet.charAt(randomChar);
-  }
-
-  const domain = domains[Math.floor(Math.random() * domains.length)];
-
-  return name + domain;
-}
-
-/**
- * Receives available domains
- * @returns {Promise.<Array, Error>}
- */
-function getAvailableDomains() {
-  return get(`${API_URL}/request/domains/format/json/`).then(JSON.parse);
-}
-
-/**
- * Generates email on temp-mail.ru
- * @param {number} [len]
- * @param {string} prefix
- * @returns {Promise.<String, Error>}
- */
-function generateEmail(len, prefix) {
-  return getAvailableDomains()
-    .then(availableDomains => getRandomEmail(availableDomains, len, prefix));
-}
-
-/**
- * Receives inbox from temp-mail.ru
- * @param {string} email
- * @returns {Promise.<(Object|Array), Error>}
- */
-function getInbox(email) {
-  if (!email) {
-    throw new Error('Please specify email');
-  }
-
-  return get(`${API_URL}/request/mail/id/${getEmailHash(email)}/format/json/`).then(JSON.parse);
-}
-
-function deleteMail(mailId) {
-  return new Promise((resolve, reject) => {
-    if (!mailId) {
-      return reject('Please specify mail identifier');
-    }
-
-    return get(`${API_URL}/request/delete/id/${mailId}/format/json/`).then(JSON.parse);
-  });
-}
-
-module.exports = { generateEmail, getInbox, deleteMail };
+module.exports = TempMail;
